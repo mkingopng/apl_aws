d5e7b62d5935efdd0ad4acf4f48b8f4355eb2c80
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2023-12-28 22:28:23 +1000

    functions to create dynamodb and add records to dynamodb

---
+++ meet_entry_web_form/db_operations.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+from functions import *
+from variables import *
+
+
+if __name__ == '__main__':
+	# replace 'YourTableName' with your desired table name
+	# create the db
+	create_dynamodb_table(f'{table_name}')
--- meet_entry_web_form/meet_entry.py
+++ meet_entry_web_form/meet_entry.py
@@ -4,110 +4,88 @@ simple web-based entry form to collect lifter's data for meet.
 output is JSON to replicate expected output from EventBrite API
 
 """
-import streamlit as st
-import re
-from datetime import date
-import pandas as pd
 import os
+import streamlit as st
+from functions import *
+from variables import *
 
 
-data_path = './../data'
+if __name__ == '__main__':
+    # title
+    st.title("Australian Powerlifting League \nMeet Registration")
 
-# Define the DataFrame structure
-columns = ["First Name", "Last Name", "Email", "Phone Number", "Gender", "Equipment", "Event", "Date of Birth", "Next of Kin Name", "Next of Kin Phone Number"]
-data = pd.DataFrame(columns=columns)
+    # creating form fields
+    with st.form(key='registration_form'):
+        first_name = st.text_input("First Name")
 
+        last_name = st.text_input("Last Name")
 
-def validate_email(email):
-    """
-    This function checks if the provided email string matches the pattern of a
-    standard email address. It uses regex to ensure that the email address
-    consists of characters allowed in the local part of the email, followed by
-    an '@' symbol, and a domain part which includes domain labels separated by
-    periods. The domain labels cannot start or end with hyphens.
+        email = st.text_input(
+            "Email",
+            help="Enter a valid email address"
+        )
 
-    :param email: (str) The email address to be validated.
-    :return: (bool) True if email address is in valid format, else False.
-    """
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return re.match(pattern, email) is not None
+        phone_number = st.text_input("Mobile Phone Number")
 
+        gender = st.selectbox(
+            "Gender",
+            options=["Male", "Female"]
+        )
 
-def validate_phone_number(phone):
-    """
-    Validate a phone number to ensure it has 10 digits and starts with 0.
-    This function uses regular expression to validate the phone number format.
-    The phone number is expected to be a string or an integer. It first
-    converts the phone number to a string (if it isn't already), then checks if
-    it matches the pattern of starting with zero followed by exactly nine other
-    digits.
+        equipment = st.selectbox(
+            "Equipment",
+            options=[
+                "Sleeves",
+                "Wraps",
+                "Single-ply"
+            ]
+        )
 
-    :param phone: (str or int): The phone number to be validated.
-    :return bool: True if the phone number is valid, False otherwise.
-    """
-    pattern = r'^0\d{9}$'
-    return re.match(pattern, str(phone)) is not None
+        event = st.selectbox(
+            "Event",
+            options=[
+                "SBD",
+                "Bench Only",
+                "Deadlift Only"
+            ]
+        )
 
+        dob = st.date_input("Date of Birth")
 
-def validate_dob(dob):
-    """
-    This function checks if the provided date of birth (dob) is a valid date
-    not in the future. It compares the dob against the current date. The
-    function assumes that the dob is provided as a date object. If the dob is
-    today or in the past, the function returns True, indicating it's valid. If
-    the dob is in the future, it returns False, indicating an invalid date of
-    birth.
+        nok_name = st.text_input("Next of Kin Name")
 
-    :param dob: (Date) The date of birth to be validated. Expected to be a date
-    object.
-    :return: (Bool) True if the date of birth is today or in the past, False if
-    it is in the future
-    """
-    today = date.today()
-    return dob <= today
+        nok_phone = st.text_input("Next of Kin Mobile Phone Number")
 
+        # submit button
+        submit_button = st.form_submit_button(label="Submit")
 
-# title
-st.title("Australian Powerlifting League Meet Registration")
+        if submit_button:
+            if validate_email(email) and validate_phone_number(phone_number):
+                # create a new record as a df
+                new_entry_df = pd.DataFrame([{
+                    "First Name": first_name,
+                    "Last Name": last_name,
+                    "Email": email,
+                    "Phone Number": phone_number,
+                    "Gender": gender,
+                    "Equipment": equipment,
+                    "Event": event,
+                    "Date of Birth": dob,
+                    "Next of Kin Name": nok_name,
+                    "Next of Kin Phone Number": nok_phone
+                }])
 
-# Creating form fields
-with st.form(key='registration_form'):
-    first_name = st.text_input("First Name")
-    last_name = st.text_input("Last Name")
-    email = st.text_input("Email", help="Enter a valid email address")
-    phone_number = st.text_input("Mobile Phone Number")
-    gender = st.selectbox("Gender", options=["Male", "Female"])
-    equipment = st.selectbox("Equipment", options=["Sleeves", "Wraps", "Single-ply"])
-    event = st.selectbox("Event", options=["SBD", "Bench Only", "Deadlift Only"])
-    dob = st.date_input("Date of Birth")
-    nok_name = st.text_input("Next of Kin Name")
-    nok_phone = st.text_input("Next of Kin Mobile Phone Number")
+                # concatenate the new record with the existing df
+                data = pd.concat([data, new_entry_df], ignore_index=True)
 
-    # Submit button
-    submit_button = st.form_submit_button(label="Submit")
+                # save to JSON
+                data.to_json(
+                    os.path.join(DATA_PATH, "registration_data.json"),
+                    index=False
+                )
 
-    if submit_button:
-        if validate_email(email) and validate_phone_number(phone_number):
-            # Create a new record as a DataFrame
-            new_entry_df = pd.DataFrame([{
-                "First Name": first_name,
-                "Last Name": last_name,
-                "Email": email,
-                "Phone Number": phone_number,
-                "Gender": gender,
-                "Equipment": equipment,
-                "Event": event,
-                "Date of Birth": dob,
-                "Next of Kin Name": nok_name,
-                "Next of Kin Phone Number": nok_phone
-            }])
+                # save_to_dynamodb('YourTableName', data)
 
-            # Concatenate the new record with the existing DataFrame
-            data = pd.concat([data, new_entry_df], ignore_index=True)
-
-            # Save to JSON
-            data.to_json(
-                os.path.join(data_path, "registration_data.json"),
-                index=False
-            )
-            st.success("Registration successful!")
+                st.success("Registration successful!")
+            else:
+                st.error("Please enter a valid email address and phone number")
