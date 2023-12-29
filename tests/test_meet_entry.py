5a9c99251a13311010889c7a3d3f94c710d98a95
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2023-12-29 15:52:00 +1000

    trying make github CI actions work

--- tests/test_meet_entry.py
+++ tests/test_meet_entry.py
@@ -1,8 +1,16 @@
 """
 
 """
+from unittest.mock import patch
 import pytest
 from entry_form.meet_entry import StreamlitApp
+
+
+@pytest.fixture(autouse=True)
+def mock_dynamodb_handler():
+    with patch('entry_form.meet_entry.DynamoDBHandler') as MockHandler:
+        MockHandler.return_value = MockHandler()  # Mock instance
+        yield
 
 
 # Example of testing non-UI logic in StreamlitApp
