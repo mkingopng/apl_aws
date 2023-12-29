721c84ed388f2866617d4d4ad884aaee6039b805
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2023-12-29 15:23:31 +1000

    adding GitHub actions workflows to automate testing

--- tests/test_dynamodb_utilities.py
+++ tests/test_dynamodb_utilities.py
@@ -7,15 +7,20 @@ from entry_form.dynamodb_utilities import DynamoDBHandler
 
 # Example of testing DynamoDBHandler's create_table method
 def test_create_table():
+    """
+
+    :return:
+    """
     with patch('entry_form.dynamodb_utilities.boto3') as mock_boto3:
         mock_dynamodb = Mock()
         mock_boto3.resource.return_value = mock_dynamodb
 
-        # Setting up mock return value for list_tables()
+        # setting up mock return value for list_tables()
         mock_dynamodb.meta.client.list_tables.return_value = {'TableNames': ['existing_table', 'another_table']}
 
         handler = DynamoDBHandler("test_table")
         handler.create_table()
 
-        # Assertions
+        # assertions
         mock_dynamodb.create_table.assert_called_once()
+        # todo: add more tests
--- tests/test_meet_entry.py
+++ tests/test_meet_entry.py
@@ -7,5 +7,10 @@ from entry_form.meet_entry import StreamlitApp
 
 # Example of testing non-UI logic in StreamlitApp
 def test_streamlit_app_logic():
+    """
+
+    :return:
+    """
     # Implement tests for non-UI methods of StreamlitApp
     pass
+    # todo: add more tests
