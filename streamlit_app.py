ab2de008737e7c24c29f041749ffd4cdb8260fdf
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2024-02-10 13:46:27 +1000

    a few little tweaks to make it a multi page app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,14 @@ import streamlit as st
 
 # This file can be kept minimal since Streamlit automatically handles page navigation
 st.title('Australian Powerlifting League Analysis')
-st.write('Please select a page from the left sidebar to view different analyses.')
+st.write("""
+This is a series of questions i found myself asking as i looked at the Open Powerlifting data. 
+\nEach page attempts to answer a question stated in the plot tite, using plots of the data from Open Powerlifting.
+\nThe plots are interactive: however you mouse over the plot elements to see hover text showing details of the data.
+\n This is a work in progress so expect there to be ongoing additions of new questions and plots
+\nPlease select a page from the left sidebar to view different analyses.
+""")
+
 
 
 
